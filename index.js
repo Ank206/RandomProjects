cbe00dfffe2db7bf4dd854e373d36d0352497f25
Ank206 <akapoor1_be22@thapar.edu>
2023-10-28 12:57:31 +0530

    New Theme

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ window.onmousemove = (e) => {
     parseFloat(track.dataset.prevPercentage) + percentage;
   const nextPercentage = Math.max(
     Math.min(nextPercentageUnconstrained, 0),
-    -100
+    -80
   );
 
   track.dataset.percentage = nextPercentage;
@@ -33,18 +33,18 @@ window.onmousemove = (e) => {
     { duration: 1200, fill: "forwards" }
   );
 
-  for (const image of track.getElementsByClassName("image")) {
-    // image.style.objectPosition = `${-nextPercentage}% 50%`;
-    image.animate(
-      {
-        objectPosition: `${nextPercentage + 100}% 50%`,
-      },
-      {
-        duration: 1200,
-        fill: "forwards",
-      }
-    );
-  }
+  // for (const image of track.getElementsByClassName("image")) {
+  //   // image.style.objectPosition = `${-nextPercentage}% 50%`;
+  //   image.animate(
+  //     {
+  //       objectPosition: `${nextPercentage + 100}% 50%`,
+  //     },
+  //     {
+  //       duration: 1200,
+  //       fill: "forwards",
+  //     }
+  //   );
+  // }
 };
 
 // document.querySelectorAll(".word").forEach((h1) => {
@@ -57,41 +57,41 @@ window.onmousemove = (e) => {
 //     e.target.style.color = "white";
 //   };
 // });
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const elements = document.querySelectorAll(".word");
+// function makeRed(event) {
+//   // event.target.style.color = "red";
+//   event.target.style.opacity = 1;
+//   // event.target.children[0].style.color = "red";
+//   const childElements = event.target.querySelectorAll(".letter");
+//   childElements.forEach((childElement) => {
+//     childElement.style.opacity = 1;
+//   });
 
-const elements = document.querySelectorAll(".word");
-function makeRed(event) {
-  // event.target.style.color = "red";
-  event.target.style.opacity = 1;
-  // event.target.children[0].style.color = "red";
-  const childElements = event.target.querySelectorAll(".letter");
-  childElements.forEach((childElement) => {
-    childElement.style.opacity = 1;
-  });
+//   elements.forEach((element) => {
+//     if (element !== event.target) {
+//       // element.style.color = "green";
+//       element.style.opacity = 0.3;
+//     }
+//   });
+// }
+// function back(event) {
+//   // event.target.style.color = "white";
+//   event.target.style.opacity = 1;
 
-  elements.forEach((element) => {
-    if (element !== event.target) {
-      // element.style.color = "green";
-      element.style.opacity = 0.3;
-    }
-  });
-}
-function back(event) {
-  // event.target.style.color = "white";
-  event.target.style.opacity = 1;
+//   elements.forEach((element) => {
+//     if (element !== event.target) {
+//       // element.style.color = "white";
+//       element.style.opacity = 1;
+//     }
+//   });
+// }
 
-  elements.forEach((element) => {
-    if (element !== event.target) {
-      // element.style.color = "white";
-      element.style.opacity = 1;
-    }
-  });
-}
-
-elements.forEach((element) => {
-  element.addEventListener("mouseover", makeRed);
-  element.addEventListener("mouseout", back);
-});
-
+// elements.forEach((element) => {
+//   element.addEventListener("mouseover", makeRed);
+//   element.addEventListener("mouseout", back);
+// });
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // document.querySelectorAll(".word").forEach((h1) => {
 //   h1.onmouseout = (e) => {
 //     e.target.style.color = "white";
